be246426a500352227870be8661e9ce89afcacf1
1101101011 <ilardecharles@gmail.com>
2026-06-29 15:53:06 +0800

    fix: kernel module error

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,19 +1,21 @@
 { pkgs, ... }:
 {
-  boot.loader = {
+  boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    # systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      #efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
-    };
-    grub = {
-      enable = true;
-      enableCryptodisk = false;
-      device = "nodev";
-      efiSupport = true;
-      useOSProber = true;
-      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+    loader = {
+      # systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        #efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+      };
+      grub = {
+        enable = true;
+        enableCryptodisk = false;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      };
     };
   };
 }
